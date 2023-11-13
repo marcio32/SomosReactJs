9e8ebb7189228607b25a98517eef72826d6c3cdd
Marcio Abriola <marcioabriola@gmail.com>
2023-11-13 20:12:32 -0300

    subo material

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line react/prop-types
+export const HeroesApp = (props) => {
+  return (
+    <p>
+      {console.log(props)}
+      {props}
+    </p>
+  );
+};
+
+export const HeroesApp2 = () => {
+  return <p>Hola Mundo2</p>;
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HeroesApp } from './HeroesApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>Hola</p>
+    <HeroesApp hola={'hola mundo'} />
   </React.StrictMode>
 );
